da5af7c666d61b5178706e49d0599923c487565e
Oliver Hdz <olivermanuelh@gmail.com>
2023-08-01 22:38:44 -0600

    First Configuration NO Database

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+//import exphbs from 'express-handlebars'
+import path from 'path'
+import indexRoutes from './routes/index.routes'
+
+const app = express()
+const exphbs = require('express-handlebars').engine
+
+//Indicar a express donde esta la carpeta views
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine(".hbs",exphbs({
+        layoutsDir: path.join(app.get('views'), 'layouts'),
+        deafaultLayout: "main", 
+        extname: ".hbs", 
+    })
+);
+
+//definir motor de plantilla
+app.set('view-engine', '.hbs')
+
+//routes
+app.use(indexRoutes)
+
+export default app;
